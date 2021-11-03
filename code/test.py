949fbe53389d2369fcf2b0c36d36376618672f7a
gabrielalusiada <13001228@lis.ulusiada.pt>
2021-11-03 12:38:22 +0000

    Atualização da app com base de dados

---
+++ code/test.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+# 1o passo: conexão
+connection =  sqlite3.connect('data.db') # sqlite armazenar toda informação da base de dados neste arquivo
+
+# 2o passo: cursor
+cursor = connection.cursor()
+
+# 3o passo: queries
+#create_table = "CREATE TABLE users (id int, username text, password text)"
+#cursor.execute(create_table)
+
+# armazenar dados na base de dados
+#user = (1, 'joao', '1234')
+#insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+#cursor.execute(insert_query, user)
+
+# inserir múltiplas linhas
+#users = [(2, 'gabriela', 'azul'),
+#         (3, 'pedro', 'verde')]
+#cursor.executemany(insert_query, users)
+
+# como recuperamos dados
+select_query = "SELECT * FROM users"
+for row in cursor.execute(select_query):
+    print(row)
+
+connection.commit()
+connection.close()
